a61f6a8e954b6b26954aa6eabd2fb44408212871
caiolucass <caiolfsantoss@gmail.com>
2021-12-15 12:01:23 -0300

    categoriesRoutes

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,19 @@
+import {Router} from "express";
+import {v4 as uuidV4} from "uuid";
+
+const categoriesRoutes = Router();
+const categories = [];
+
+categoriesRoutes.post("/", (request, response) =>{
+  const {name, description} = request.body;
+  const category = {
+    id: uuidV4(),
+    name,
+    description,
+  };
+  categories.push(category);
+
+  return response.status(201).json({sucess:"Categoria " + `${name}` + " criada com sucesso!"}).send();
+});
+
+export {categoriesRoutes};
